7eb47c25d11ef7f5a446bc6144ee458b8f749386
jonathansanger <jonathan@pixvana.com>
2022-04-20 20:36:50 -0700

    create base app

---
+++ AudioRecorder/AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Jonathan Sanger on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
